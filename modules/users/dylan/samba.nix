98a52bb7a5fa17427c59fd61719e9cdd76ac7afd
hierocles <4733259+hierocles@users.noreply.github.com>
2026-01-31 00:32:04 +0000

    update activation script

--- modules/users/dylan/samba.nix
+++ modules/users/dylan/samba.nix
@@ -3,13 +3,14 @@ _: {
     nixos = {
       config,
       lib,
+      pkgs,
       ...
     }: {
       services.samba.customShares.media.validUsers = lib.mkIf config.services.samba.enable ["dylan"];
 
       system.activationScripts = lib.mkIf config.services.samba.enable {
         init_smbpasswd.text = ''
-          /run/current-system/sw/bin/printf "$(/run/current-system/sw/bin/cat ${config.sops.secrets."passwords/dylan".path})\n$(/run/current-system/sw/bin/cat ${config.sops.secrets."passwords/dylan".path})\n" | /run/current-system/sw/bin/smbpasswd -sa dylan
+          printf "$(<${config.sops.secrets."passwords/dylan".path})\n$(<${config.sops.secrets."passwords/dylan".path})\n" | ${pkgs.samba}/bin/smbpasswd -sa dylan
         '';
       };
     };
