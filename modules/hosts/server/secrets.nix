00ce503e9a12147f3d80d40f22904f3f21b5ea4b
hierocles <4733259+hierocles@users.noreply.github.com>
2026-01-31 00:26:00 +0000

    don't need standalone key on server

--- modules/hosts/server/secrets.nix
+++ modules/hosts/server/secrets.nix
@@ -7,8 +7,8 @@
 
         age = {
           sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-          keyFile = "/var/lib/sops-nix/key.txt";
-          generateKey = true;
+          #keyFile = "/var/lib/sops-nix/key.txt";
+          #generateKey = true;
         };
 
         secrets = {
