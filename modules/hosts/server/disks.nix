262dd4fd395aa00427115b57618a7b0d6e028d9f
hierocles <4733259+hierocles@users.noreply.github.com>
2026-01-31 02:02:12 +0000

    update disk

--- modules/hosts/server/disks.nix
+++ modules/hosts/server/disks.nix
@@ -1,23 +1,32 @@
 {inputs, ...}: {
   flake.aspects.server._.disks = {
-    nixos = {...}: let
-      inherit (inputs.self) lib;
+    nixos = {lib, ...}: let
+      flakeLib = inputs.self.lib;
     in {
       # Disk Layout:
       # - NVMe drive: 1.9TB BTRFS for NixOS system (managed by disko)
       # - ZFS pool "media": 4x10TB HDDs in striped mirror (NOT managed by disko)
 
-      disko.devices = lib.mkBtrfsDiskLayout {
+      disko.devices = flakeLib.mkBtrfsDiskLayout {
         swapsize = 32; # 32GB swap
         device = "/dev/disk/by-id/nvme-TEAM_TM8FP6002T_TPBF2312080030304554";
       };
+
+      # Override /home mount options to remove erroneous 'bind' option from disko
+      fileSystems."/home".options = lib.mkForce [
+        "defaults"
+        "compress-force=zstd"
+        "noatime"
+        "ssd"
+        "subvol=home"
+      ];
 
       boot.zfs.extraPools = ["media"];
 
       fileSystems."/mnt/media" = {
         device = "media";
         fsType = "zfs";
-        options = ["zfsutil"];
+        options = ["zfsutil" "nofail" "x-systemd.requires=zfs-import.target"];
       };
     };
   };
